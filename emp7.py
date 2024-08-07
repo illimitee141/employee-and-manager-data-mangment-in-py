b07086f7a373219641257780d1c91eaa2e09cec2
AMIT YADAV <ay3453796@gmail.com>
2024-08-07 09:42:39 +0530

    Add files via upload

---
+++ emp7.py
@@ -0,0 +1,25 @@
+import manager7
+
+print(manager7.rohit_id)
+print(manager7.rohit_pass)
+
+print(manager7.emp_list)
+
+calling
+manager7.rohit_hi()
+
+
+
+
+import manager7 as mng #inck name of manager7 is mng
+print(mng.rohit_id)
+print(mng.rohit_pass)
+print(mng.man_id)
+
+print(mng.emp_list)
+
+calling
+mng.rohit_hi()
+
+from manager7 import rohit_id
+print(rohit_id)
---
+++ manager7.py
@@ -0,0 +1,21 @@
+man_name = "mohan"
+man_add = "jaipur"
+
+emp_list = ['rohit','rohan','akashy']
+rohit_id = 'hello.com'
+rohit_pass = "129xxxxx"
+
+def rohit_hi():
+    print("employee name ","rohit")
+    print(rohit_id)
+    print(rohit_pass)
+
+if __name__ == 
+def man_hi():
+    print("manager name ","mohan")
+    print(man_id)
+    print(man_pass)
+
+
+man_id = 'man.com'
+man_pass = "1828xxxx"
